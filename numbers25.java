1024d5e7a1b119e6ba39a6e2b3834ba819e4f6d7
xavierzaidane <xavierzaidane@gmail.com>
2023-11-17 15:08:18 +0700

    after change

--- numbers25.java
+++ numbers25.java
@@ -1,11 +1,18 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class numbers25 {
     public static void main(String[] args) {
-Scanner scan = new Scanner (System.in);
-int[][] myNumbers = new int [3][];
-myNumbers[0] = new int [5];
-myNumbers [1] = new int [3];
-myNumbers [2] = new int [1];
-} 
-}
-    
+        Scanner scan = new Scanner (System.in);
+        int[][] myNumbers = new int [3][];
+        myNumbers[0] = new int [5];
+        myNumbers [1] = new int [3];
+        myNumbers [2] = new int [1];
+        for(int i = 0;i<myNumbers.length;i++){
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+        for(int i = 0;i<myNumbers.length;i++){
+            System.out.println("Length of row " + (i+1) + ": " +myNumbers[i].length);
+        }
+        }
+        
+        }
