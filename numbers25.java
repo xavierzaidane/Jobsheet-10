11206e83c0c1d1053e1eb4b3deff7f55459db1e2
xavierzaidane <xavierzaidane@gmail.com>
2023-11-17 15:06:07 +0700

    update numbers

---
+++ numbers25.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class numbers25 {
+    public static void main(String[] args) {
+Scanner scan = new Scanner (System.in);
+int[][] myNumbers = new int [3][];
+myNumbers[0] = new int [5];
+myNumbers [1] = new int [3];
+myNumbers [2] = new int [1];
+} 
+}
+    
