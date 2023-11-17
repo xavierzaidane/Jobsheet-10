66d24c5ed8c744589bce004019994f8637825c19
xavierzaidane <xavierzaidane@gmail.com>
2023-11-17 14:47:22 +0700

    update

--- cinemawithscanner25.java
+++ cinemawithscanner25.java
@@ -17,11 +17,10 @@ public class cinemawithscanner25 {
         System.out.printf("%s \t %s \n",audience[3][0],audience[3][1]);
         System.out.println(audience.length);
         
-        for(int i = 0;i<audience.length;i++){
-        System.out.println("Length of row" +(i+1) + ":" + audience[i].length);
-        
-        }
-        
+        for(String[]rowAudience:audience){
+            System.out.println("Length of row  : " +rowAudience.length);
+            }
+            
         
         
         }    
